42832b1dc8c721d1b376d9aca5e046927c165a14
Nikhil Bhatt <bhattnikhil158@gmail.com>
2025-09-07 23:29:58 +0530

    Update

--- Think/server/controllers/messageController.js
+++ Think/server/controllers/messageController.js
@@ -1,13 +1,108 @@
-import Chat from "../models/Chat.js"
+import Chat from "../models/chat.js"
+import User from "../models/user.js"
+import openai from "../configs/openai.js"
+import axios from "axios"
+import imagekit from "../configs/imageKit.js"
 //Text based AI chat messgae controller
 
-export const textMessageController = async (resizeBy, res) => {
-    try{
-        const userId = requestAnimationFrame.user._id
-        const {chatId, prompt} = req.body 
+export const textMessageController = async (req, res) => {
+    try {
+        const userId = req.user._id
 
-        const chat = await Chat.findOne({userId, id: chatId})
-    }catch(error){
+        //check credit
+        if(req.user.credits < 1){
+            return res.json({success:false, message:"You don't have enough credits to use this feature"})
+        }
+        const { chatId, prompt } = req.body
+
+        const chat = await Chat.findOne({ userId, _id: chatId })
+        chat.messages.push({ role: "user", content: prompt, timestamp: Date.now(), isImage: false })
+
+
+        const {choices} = await openai.chat.completions.create({
+            model: "gemini-2.0-flash",
+            messages: [
+                {
+                    role: "user",
+                    content: prompt,
+                },
+            ],
+        });
+
+        const reply = {...choices[0].message,timestamp: Date.now(), isImage: false }
+        res.json({success:true, reply})
+
+        chat.messages.push(reply)
+        await chat.save()
+        await User.updateOne({_id: userId}, {$inc: {credits: -1}})
+
+
+    } catch (error) {        
+        res.json({success:false, message:error.message})
+
 
     }
 }
+
+
+
+//Image Genreation Message Controller
+ export const imageMessageController = async (req, res) => {
+    try{
+        const userId = req.user._id;
+        //check credits
+        if(req.user.credits < 2){
+            return res.json({success: false, message: "You don't have enough credits to use this feature"})
+        
+        }
+        const {prompt, chatId, isPublished} = req.body
+        //find chat
+        const chat = await Chat.findOne({userId, _id: chatId})
+
+        //push user message
+        chat.messages.push({
+            role:"user",
+            content: prompt,
+            timestamp: Date.now(),
+            isImage: false });
+
+            //Encoded the prompt
+            const encodedPrompt = encodeURIComponent(prompt)
+
+            // Construct Imagekit AI generation URL
+            const generatedImageUrl =`${process.env.IMAGEKIT_URL_ENDPOINT}/ik-genimg-prompt-${encodedPrompt}/thing/${Date.now()}.png?tr=w-800,h-800`;
+
+            //triger generation by detcging from ImageKit
+            const aiImageResponse =
+            await axios.get(generatedImageUrl,{responseType: "arraybuffer"})
+
+            // Convert to Base64
+            const base64Image =`data:image/png;base64,${Buffer.from(aiImageResponse.data,"binary").toString('base64')}`;
+
+            //Upload to ImageKit Media Library
+            const uploadResponse = await imagekit.upload({
+                file:base64Image,
+                fileName:`${Date.now()}.png`,
+                folder:"thing"
+            })
+            
+               const reply = {
+                role:'assistant',
+                content: uploadResponse.url,
+                timestamp: Date.now(),
+                isImage: true,
+                isPublished }
+        res.json({success:true, reply})
+
+        chat.messages.push(reply)
+        await chat.save()
+
+        await User.updateOne({_id: userId}, {$inc: {credits: -2}})
+
+            
+
+    }catch (error) {
+        res.json({success:false, message:error.message });
+
+    }
+ }
